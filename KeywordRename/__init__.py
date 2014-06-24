577a63163190eb0a7a0fcc4ef36bfed5d6a88b3f
Alan Schmitt <alan.schmitt@polytechnique.org>
2014-06-24 12:07:21 +0200

    putting the replacement table in a dict

--- KeywordRename/__init__.py
+++ KeywordRename/__init__.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 from pygments.token import Keyword
+from pygments.token import Operator
 from pygments.filter import Filter
 
 class KWRenameFilter(Filter):
+    transdict = {'forall' : u'∀', 'exists' : u'∃', '=>' : u'⇒'}
 
     def __init__(self, **options):
         Filter.__init__(self, **options)
 
     def filter(self, lexer, stream):
         for ttype, value in stream:
-	    if ttype is Keyword and value == 'forall':
-	        value = value.replace('forall',u'∀')
-            elif ttype is Keyword and value == 'exists':
-	        value = value.replace('exists',u'∃')
+            v = value.__str__()
+	    if (ttype is Keyword or ttype is Operator) and v in self.transdict:
+	        value = value.replace(v,self.transdict[v])
             yield ttype, value
