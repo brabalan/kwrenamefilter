dbb7a09376a8ad2a159ab6d3bce96556010aa16f
Alan Schmitt <alan.schmitt@polytechnique.org>
2014-06-24 10:12:27 +0200

    trying a setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+"""
+UTF8 Coq keyword translation filter for Pygments.
+"""
+
+from setuptools import setup
+
+entry_points = """
+[pygments.filters]
+CoqKwFilter = utf8CoqFilter.utf8CoqFilter:CoqUTFFilter
+"""
+
+setup(name = 'coqkwfilter',
+  version  = '0.1',
+  description  = __doc__,
+  author       = "Alan Schmitt",
+  packages     = ['utf8CoqFilter'],
+  entry_points = entry_points)
