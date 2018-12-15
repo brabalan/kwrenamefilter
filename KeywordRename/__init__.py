c8673c413db7306023901252c2df0d9e67503b80
Alan Schmitt <alan.schmitt@polytechnique.org>
2018-12-15 11:18:30 +0100

    Python 3 compatibility
    This adds Python 3 compatibility to the filter

--- KeywordRename/__init__.py
+++ KeywordRename/__init__.py
@@ -1,17 +1,29 @@
 # -*- coding: utf-8 -*-
 from pygments.token import Keyword
 from pygments.token import Operator
+from pygments.token import Name
 from pygments.filter import Filter
 
+import sys
+
+PY2 = sys.version_info[0] == 2
+PY3 = sys.version_info[0] == 3
+
 class KWRenameFilter(Filter):
-    transdict = {'forall' : u'∀', 'exists' : u'∃', '->' : u'→', '~' : u'¬', '<->' : u'↔', '=>' : u'⇒', '/\\' : u'∧', '\\/' : u'∨'}
+    if PY2:
+        transdict = {'forall' : u'∀', 'exists' : u'∃', '->' : u'→', '~' : u'¬', '<->' : u'↔', '=>' : u'⇒', '/\\' : u'∧', '\\/' : u'∨'}
+    if PY3:
+        transdict = {'forall' : '∀', 'exists' : '∃', '->' : '→', '~' : '¬', '<->' : '↔', '=>' : '⇒', '/\\' : '∧', '\\/' : '∨'}
 
     def __init__(self, **options):
         Filter.__init__(self, **options)
 
     def filter(self, lexer, stream):
         for ttype, value in stream:
-            v = value.encode('utf-8').__str__()
-	    if (ttype is Keyword or ttype is Operator) and v in self.transdict:
-	        value = value.replace(v,self.transdict[v])
+            if PY2:
+                v = value.encode('utf-8').__str__()
+            if PY3:
+                v = value.__str__()
+            if (ttype is Keyword or ttype is Operator) and v in self.transdict:
+                value = value.replace(v,self.transdict[v])
             yield ttype, value
